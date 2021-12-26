1843f618c33aea073150a74548b54936aaa72eac
send2durai <send2durai@hotmail.com>
2021-12-27 01:09:19 +0530

    tfa.sh has modified

--- shell-scripts/tfa.sh
+++ shell-scripts/tfa.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
 echo "Simple Script to apply OR destory the infrastructure stack provisioned by Terraform(Iac)"
 sleep 2
-echo "Please enter 1 to creating infra stack in aws"
-echo "Please enter 2 to destroying infra stack in aws"
-read -p "Enter the number to create/destroy:" NUMBER
+echo "Action,READY, STEDY, GO"
+sleep 5
+echo "Please enter 1 to initializing terraform to download the require plugins for the CLOUD PROVIDER"
+echo "Please enter 2 to format the code that we written in HCL"
+echo "Please enter 3 to validate the code that we written in HCL"
+echo "Please enter 4 to apply the code to provision infra in AWS"
+echo "Please enter 5 to destroy the infra that we provisioned via terraform"
+sleep 2
+read -p "Enter the number to perform these actions one after another:" NUMBER
 if [ $NUMBER -eq 1 ]; then
-        terraform apply --auto-approve
+        terraform init
 elif [ $NUMBER -eq 2 ]; then
-        terraform destroy --auto-approve
+        terraform fmt
+elif [ $NUMBER -eq 3 ]; then
+        terraform validate
+elif [ $NUMBER -eq 4 ]; then
+        terraform apply --auto-approve
+elif [ $NUMBER -eq 5 ]; then
+        terraform destory --auto-approve
 else
-        echo "You should choose the number 1 or 2"
+        echo "You should choose the number between 1 to 5"
 fi
+
