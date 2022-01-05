0848086f253740ca68f850feef7b7d42517f2c1d
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-01-05 10:21:16 +0530

    Create create-k8s-ns-pod.sh
    Initial commit

---
+++ shell-scripts/create-k8s-ns-pod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo " ************ Automate on K8's Pods and Namespace Creation *************"
+sleep 2
+read -p "Enter the Namespace, you'd like to create in K8's:" NS
+echo -e " you entered name is : $NS"
+sleep 2
+kubectl create ns $NS
+echo "Going to see namespace is created or not:"
+kubectl get ns
+sleep 2
+read -p "Enter the Pod Path:" PODPATH
+echo -e "you entered name is : $PODPATH"
+sleep 2
+kubectl apply -f $PODPATH
+sleep 3
+kubectl get pods -n $NS
+sleep 3
+kubectl describe pods $NS-pod -n $NS
+sleep 3
+exit
