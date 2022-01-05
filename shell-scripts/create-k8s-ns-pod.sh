b70a18259d73d2aae80e2e5e02932c182e4e7e9f
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-01-05 11:33:18 +0530

    Update create-k8s-ns-pod.sh

--- shell-scripts/create-k8s-ns-pod.sh
+++ shell-scripts/create-k8s-ns-pod.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-echo " ************ Automate on K8's Pods and Namespace Creation *************"
-sleep 2
-read -p "Enter the Namespace, you'd like to create in K8's:" NS
-echo -e " you entered name is : $NS"
-sleep 2
+echo " *********** Going to create pod vs namespace in kubernetes ************"
+sleep 3
+read -p "Enter the namespace, that you wanted to create:" NS
+echo -e "Make sure and confirm that you wanted to create namespace as:$NS"
 kubectl create ns $NS
-echo "Going to see namespace is created or not:"
-kubectl get ns
 sleep 2
-read -p "Enter the YAML file Path:" YAMLPATH
-echo -e "you entered name is : $YAMLPATH"
-sleep 2
-kubectl apply -f $PODPATH
+kubectl get ns 
 sleep 3
+read -p "Please enter the YAML file path to create pod in K8's :" YAMLPATH
+echo -e "You've entered the yaml path is as: $YAMLPATH"
+sleep 3
+kubectl apply -f $YAMLPATH
+sleep 2
 kubectl get pods -n $NS
-sleep 3
-kubectl describe pods $NS-pod -n $NS
+sleep 2
+kubectl describe pods $NS -n $NS
 sleep 3
 exit
