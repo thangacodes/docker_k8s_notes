a6c6c3ffae8231cfd70f7e12233273a9bcc467c0
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-02-16 18:56:18 +0530

    Create minikube-stop.sh
    initial commit

---
+++ shell-scripts/minikube-stop.sh
@@ -0,0 +1,14 @@
+### Minikube stopping on virtual machine hosted on VirtualBox
+
+#!/bin/bash
+echo -e "\e[1;31m ******** STOPPING MINIKUBE ON LOCAL LAPTOP ************"
+minikube stop
+echo -e "\e[1;37m ******* BREAK ********"
+echo -e "\e[1;30m ******* BREAK ********"
+echo -e "\e[1;32m ******* BREAK ********"
+echo -e "\e[1;33m ******* BREAK ********"
+echo -e "\e[1;31m ******* BREAK ********"
+echo -e "\e[1;34m ******* BREAK ********"
+echo -e "\e[1;34m ****************** Checking the minikube status on local laptop ********************"
+minikube status
+exit
