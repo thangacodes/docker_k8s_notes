a5641e2283aca24819148f74ce9f5695c05a3323
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-02-16 18:53:06 +0530

    Create minikube-start.sh
    initial commit

---
+++ shell-scripts/minikube-start.sh
@@ -0,0 +1,17 @@
+## This script is used to START minikube on Ubuntu OS, which is hosted on Virtual Box
+
+#!/bin/bash
+echo -e "\e[1;31m ******** STARTING MINIKUBE IN LOCAL LAPTOP ************"
+
+#minikube start
+
+echo -e "\e[1;37m ******* BREAK ********"
+echo -e "\e[1;30m ******* BREAK ********"
+echo -e "\e[1;32m ******* BREAK ********"
+echo -e "\e[1;33m ******* BREAK ********"
+echo -e "\e[1;31m ******* BREAK ********"
+echo -e "\e[1;34m ******* BREAK ********"
+echo -e "\e[1;34m ****************** Checking the minikube status on local laptop ********************"
+sleep 2
+minikube status
+exit
